f7a702f8f8c4db54a81ac156423fdd8b236a05f6
shruti kumari <104879589+Its-shruti@users.noreply.github.com>
2024-10-03 16:07:11 +0530

    json folder

---
+++ jsonData/project.js
@@ -0,0 +1,38 @@
+export const arrData = [
+    {
+        "title": "E-commerce Analysis",
+        "detail": "Analyzed customer demographics and purchasing behavior to create targeted customer segments.",
+        "technology": "Pandas, Numpy, Matplotlib, Seaborn",
+        "alt": "image"
+    },
+    {
+        "title": "Student Result Analysis",
+        "detail": " Identify subjects where students have shown significant growth or stagnation.",
+        "technology": "Pandas, Numpy, Matplotlib, Seaborn",
+        "alt": "image"
+    },
+    {
+        "title": "Digital Music Store Analysis",
+        "detail": "Identify how to analyze music playlist database, its business growth.",
+        "technology": "SQL",
+        "alt": "image"
+    },
+    {
+        "title": "Vrinda Store Data Analysis",
+        "detail": " Focused on data analysis, data cleaning, data process, data visualization.",
+        "technology": "Excel",
+        "alt": "image"
+    },
+    {
+        "title": "Gradient Changer",
+        "detail": "It allows the user to change the background gradient dynamically, Using JavaScript, you can change the background gradient based on user input, such as selecting colors or clicking button",
+        "technology": "HTML, CSS, JavaScript",
+        "alt": "image"
+    },
+    {
+        "title": "Super Store sales Dashboard",
+        "detail": " Focus on time series analysis, to provide valuable insights and accurate sales forecasting.",
+        "technology": "PowerBI",
+        "alt": "image"
+    },
+]
---
+++ jsonData/skills.js
@@ -0,0 +1,22 @@
+export const skillData = [
+    {
+        skill: "Pyhton",
+        icon_class: "fa-brands fa-python",
+        detail: "I have accquired the knowledge of Python Core, and libraries like Pandas, Numpy, Matplotlib, Seaborn"
+    },
+    {
+        skill: "PowerBI",
+        icon_class: "fa-solid fa-chart-column",
+        detail: "Power BI is a powerful business analytics tool that enables users to visualize data, share insights, and create interactive reports and dashboards, helping organizations make data-driven decisions."
+    },
+    {
+        skill: "Excel",
+        icon_class: "fa-solid fa-table",
+        detail: "Excel is a powerful spreadsheet application used for data analysis, visualization, and management. It offers features like formulas, functions, pivot tables, and charts, enabling users to organize data efficiently."
+    },
+    {
+        skill: "SQL",
+        icon_class: "fa-solid fa-database",
+        detail: "SQL (Structured Query Language) is a programming language used for managing and manipulating relational databases, enabling data retrieval, insertion, and updates."
+    },
+]
